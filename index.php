7ddf57cff0eae11eae184ab8432e199ebfddb5ef
Cailenys <cailenys.salazar@gmail.com>
2020-04-29 16:30:09 -0600

    Basic syntax

--- index.php
+++ index.php
@@ -1,42 +1,34 @@
-<!DOCTYPE html>
+<?php
+  // This file's code will execute right here in the file.
+  include './includes/datatypes.php';
+?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> PHP Example</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $myMessage; // Echo is used for outputting strings. ?></title>
 </head>
 <body>
-    <h1> PHP Example </h1>
-    <?php 
-    // Varibale names
-    $myVar = 'Hello, World!';
-    echo $myVar;
-
-    $name = 'Cailenys';
-    $name2 = 'Bob';
-    $greeting = "My name is: {$name}2";
-    echo $greeting;
-     
-    // Concatination operator
-    $fullGreeting = 'My name is :' . $name;
-    echo $fullGreeting;
-
-    /**
-     * DataTypes:
-     */
-
-    $myInteger = 54;
-    $myFloat= 38.968;
-    $myBoolean = TRUE;
-    $mySecondBoolan = false;
-    $mystring = 'Hello,world!';
-    $myObject = new stdClass ();
-    $myNull = NULL;
-    $myArray = array( $myInteger,  $myFloat, $myBoolean, $mySecondBoolan, $mystring,$myObject, $myNull);
-    
-    echo '<pre>';
-    var_dump($myArray);
-    echo '</pre>';
+  <h1><?php echo $myMessage; ?></h1>
+  <?php include './includes/navigation.php'; // We're executing this code right here... the nav will display. ?>
+  <pre>
+    <?php
+      // Var dump is great for seeing what's inside!
+      var_dump( $myOtherArray );
     ?>
+  </pre>
+  <h2>Concatenated String</h2>
+  <p>
+    <?php echo $concattedString; ?>
+  </p>
+  <h2>Difference Between Single and Double Quoted Strings</h2>
+  <h3>Single Quoted</h3>
+  <p>
+    <?php echo $mySingleQuoteHelloString; ?>
+  </p>
+  <h3>Double Quoted</h3>
+  <p>
+    <?php echo $myDoubleQuoteHelloString; ?>
+  </p>
 </body>
 </html>
