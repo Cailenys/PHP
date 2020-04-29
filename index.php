a15eef8b67658d78e34eefcf932ea3e87f5159bd
Cailenys <cailenys.salazar@gmail.com>
2020-04-28 20:43:43 -0600

    Differents form to make comments in php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First PHP Page</title>
-</head>
-<body>
-    <h1> My First PHP Page </h1>
+
     <?php 
+    // Single-line comment
+    # Bash-style single-line comment
+    /**
+     * This is a multi-line commet
+     * It can be
+     * many lines
+     */
     echo 'Hello, World!';
-    ?>
-</body>
-</html>
+    
