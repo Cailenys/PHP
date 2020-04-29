086ec6e58f97241805758679e9ba000048b50020
Cailenys <cailenys.salazar@gmail.com>
2020-04-28 21:22:35 -0600

    Echo, Variables, Data-Types and Var_Dump

--- index.php
+++ index.php
@@ -8,8 +8,35 @@
 <body>
     <h1> PHP Example </h1>
     <?php 
-    echo 'Hello, World!';
-    echo ( 2 + 5); //7
+    // Varibale names
+    $myVar = 'Hello, World!';
+    echo $myVar;
+
+    $name = 'Cailenys';
+    $name2 = 'Bob';
+    $greeting = "My name is: {$name}2";
+    echo $greeting;
+     
+    // Concatination operator
+    $fullGreeting = 'My name is :' . $name;
+    echo $fullGreeting;
+
+    /**
+     * DataTypes:
+     */
+
+    $myInteger = 54;
+    $myFloat= 38.968;
+    $myBoolean = TRUE;
+    $mySecondBoolan = false;
+    $mystring = 'Hello,world!';
+    $myObject = new stdClass ();
+    $myNull = NULL;
+    $myArray = array( $myInteger,  $myFloat, $myBoolean, $mySecondBoolan, $mystring,$myObject, $myNull);
+    
+    echo '<pre>';
+    var_dump($myArray);
+    echo '</pre>';
     ?>
 </body>
 </html>
