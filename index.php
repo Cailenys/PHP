be2d60bbb0f259385cc08db9a5d4f6c279ccfe0d
Cailenys <cailenys.salazar@gmail.com>
2020-04-28 20:53:20 -0600

    mathematical operations

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP Example</title>
+</head>
+<body>
+    <h1> PHP Example </h1>
     <?php 
-    // Single-line comment
-    # Bash-style single-line comment
-    /**
-     * This is a multi-line commet
-     * It can be
-     * many lines
-     */
     echo 'Hello, World!';
-    
+    echo ( 2 + 5); //7
+    ?>
+</body>
+</html>
